a4c0961dbedbf498773c4e5a287b172d98fc4d06
ralfJoswig <ralfjoswig@gmail.com>
2015-05-22 23:01:06 +0200

    iniz

---
+++ MiBandDataPanel/Panel.cs
@@ -0,0 +1,115 @@
+﻿/**
+ * Copyright (C) 2015 Ralf Joswig
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace MiBandDataPanel
+{
+    public partial class MiBandPanel: Panel
+    {
+        public enum DataType { Global, Detail }
+
+        protected DataType type;
+        protected MiBand.MiBand data;
+        protected TimeSpan sleepDuration;
+        protected DateTime showFrom;
+        protected DateTime showTo;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public MiBandPanel()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Daten für die Anzeige setzen
+        /// </summary>
+        /// <param name="_type"></param>
+        /// <param name="_data"></param>
+        /// <param name="_sleepDuration"></param>
+        /// <param name="_showFrom"></param>
+        /// <param name="_showTo"></param>
+        public void setData(DataType _type, MiBand.MiBand _data, TimeSpan _sleepDuration, DateTime _showFrom, DateTime _showTo)
+        {
+            // Daten übernehmen
+            type = _type;
+            data = _data;
+            sleepDuration = _sleepDuration;
+            showFrom = _showFrom;
+            showTo = _showTo;
+
+            // für die Datumsfelder die Zeit zurücksetzen
+            showFrom = new DateTime(showFrom.Year, showFrom.Month, showFrom.Day, 0, 0, 0);
+            showTo = new DateTime(showTo.Year, showTo.Month, showTo.Day, 23, 59, 59);
+            
+            // eigene Komponenten erzeugen
+            initOwnComponents();
+
+            // Anzeige aufbereiten
+            if (data != null)
+            {
+                showData();
+            }
+        }
+
+        /// <summary>
+        /// Erzeugt die benötigten Elemente
+        /// </summary>
+        protected virtual void initOwnComponents() { }
+
+        /// <summary>
+        /// Zeigt die Daten an
+        /// </summary>
+        protected virtual void showData() { }
+
+        /// <summary>
+        /// Fügt etwaige Evtenhandler hinzu
+        /// </summary>
+        public virtual void addListener() { }
+
+        /// <summary>
+        /// Nimmt Änderungen an der gewünschten Schlafdauer entgegen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="sleepTime"></param>
+        public void changeSleepTime(object sender, TimeSpan sleepTime)
+        {
+            // geänderte Zeit übernehmen
+            sleepDuration = sleepTime;
+
+            // Anzeige auffrischen
+            initOwnComponents();
+            showData();
+        }
+
+        /// <summary>
+        /// Nimmt Änderungen an am Anzeigezeitraum entgegen 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        public void changeShowFromTo(object sender, DateTime from, DateTime to)
+        {
+            // geänderte Werte übernehmen
+            showFrom = from;
+            showTo = to;
+
+            // Anzeige auffrischen
+            initOwnComponents();
+            showData();
+        }
+    }
+}
---
+++ MiBandDataPanel/dateComparer.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * Copyright (C) 2015 Ralf Joswig
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>
+ */
+
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace MiBandDataPanel
+{
+    public class dateComparer : IComparer
+    {
+        private static int sortOrderModifier = 1;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="sortOrder"></param>
+        public dateComparer(ListSortDirection sortOrder)
+        {
+            // Richtungsmodifizierer setzen
+            if (sortOrder == ListSortDirection.Descending)
+            {
+                sortOrderModifier = -1;
+            }
+            else if (sortOrder == ListSortDirection.Ascending)
+            {
+                sortOrderModifier = 1;
+            }
+        }
+
+        /// <summary>
+        /// Vergleicht zwei Datumswerte
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Compare(object x, object y)
+        {
+            // die beiden zu sortierenden Zeilen holen
+            DataGridViewRow DataGridViewRow1 = (DataGridViewRow)x;
+            DataGridViewRow DataGridViewRow2 = (DataGridViewRow)y;
+
+            // Datum liegt als Text vor, in Datum umwandeln
+            DateTime value1 = DateTime.Parse((string)DataGridViewRow1.Cells[0].Value);
+            DateTime value2 = DateTime.Parse((string)DataGridViewRow2.Cells[0].Value);
+
+            //  Datumswerte vergleichen
+            return value1.CompareTo(value2) * sortOrderModifier;
+        }
+    }
+}
